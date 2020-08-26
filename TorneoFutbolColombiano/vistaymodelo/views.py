d59a95dc7b3063026e03ce8c0b11a19d181e0116
“Garmidan” <garmidan16@gmail.com>
2020-08-26 16:03:09 -0500

    Se implemento que automaticamente hiciera un enfrentamiento de equipo solo la primera fecha

--- TorneoFutbolColombiano/vistaymodelo/views.py
+++ TorneoFutbolColombiano/vistaymodelo/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.shortcuts import redirect,render
 from vistaymodelo.models import Partidos,Equipo,Estadio,Posiciones
 from datetime import date,datetime,timedelta
+# Simulando los partidos de la fase de grupos
+from itertools import combinations
 import random
 
 # Creacion de vistas
@@ -15,13 +17,36 @@ def enfrentamientos(request):
     lista_equipo = Equipo.objects.all()
     lista_partido = Partidos.objects.all()
     conteEquip = lista_equipo.count()
-    enfrentamiento_pares = []
-    enfrentamiento_impares = []
-    conteoEquipos = lista_equipo.count() + 1
+    conteoEquipos = lista_equipo.count() - 1
     if lista_partido:
         return render(request,"enfrentamientos.html",{"listaequipo":lista_equipo,"listapartido":lista_partido})
     else:
-        return render(request,"enfrentamientos.html",{"listaequipo":lista_equipo,"listapartido":lista_partido,"conteoequipo":conteEquip,"pares":enfrentamiento_pares,"impares": enfrentamiento_impares})
+        fecha_parti = 0
+        mensajes = ""
+        for fechas in range(conteoEquipos):
+            validacion = 0
+            fecha_parti = fecha_parti + 1
+            if fecha_parti > 1:
+                for equipo1 in lista_equipo:
+                    for equipo2 in lista_equipo:
+                        if equipo1.nombre != equipo2.nombre:
+                                partido_registrar = Partidos(fecha_partido="2020-08-28",local=equipo1.nombre,visitante=equipo2.nombre, resultado_local=equipos.estadio.nombre, fecha=fecha_parti) 
+                                
+            else:
+                for equipos in lista_equipo:
+                    if validacion == 0:
+                        validacion = 1
+                        partido_registrar = Partidos(fecha_partido="2020-08-25",local=equipos.nombre,estadio_jugaran=equipos.estadio.nombre, fecha=fecha_parti) 
+                        partido_registrar.save()
+                    else: 
+                        partid = Partidos.objects.last()
+                        if partid:
+                            validacion = 0
+                            registrarvisitante = Partidos.objects.get(id = partid.id)
+                            registrarvisitante.visitante = equipos.nombre
+                            registrarvisitante.save()      
+               
+        return render(request,"enfrentamientos.html",{"listaequipo":lista_equipo,"listapartido":lista_partido,"conteoequipo":conteEquip,"mensaje":mensajes})
 
 def calendario(request):
     lista_partido = Partidos.objects.all()
@@ -61,7 +86,7 @@ def agregarresultadofinal(request):
         p.resultado_visitante = request.POST['resultvisitante']
         if request.POST['resultlocal'] > request.POST['resultvisitante']:
             elocal = Equipo.objects.get(nombre=p.local)
-            po = Posiciones.objects.get(equipo=elocal)
+            po = Posiciones.objects.get(equipo=elocal.nombre)
             if po:
                 po.puntos = po.puntos + 3
                 po.save()
@@ -85,7 +110,8 @@ def agregarresultadofinal(request):
                     polocal = Posiciones(puntos= 1, equipo=p.visitante)
                     polocal.save()
             else:
-                posic_visitante = Posiciones.objects.get(equipo=p.visitante)
+                equipvisitante = Equipo.objects.get(nombre=p.visitante)
+                posic_visitante = Posiciones.objects.get(equipo=equipvisitante.id)
                 if posic_visitante:
                     posic_visitante.puntos = polocal.puntos + 3
                     posic_visitante.save()
